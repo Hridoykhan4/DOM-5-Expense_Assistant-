f322e4badc218e778f6d2000e88d02317b387198
Hridoykhan4 <hridoykhan148385@gmail.com>
2025-02-03 22:44:30 +0600

    validation of input fields added

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,26 @@
 let count = 0;
-document.getElementById("calculate").addEventListener("click", function () {
-  count++;
+
+document.getElementById("calculate").addEventListener("click", calculateBudget);
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    calculateBudget();
+  }
+});
+
+function calculateBudget() {
   const income = getInputValueById("income");
   const software = getInputValueById("software");
   const courses = getInputValueById("courses");
   const internet = getInputValueById("internet");
 
-  if (income <= 0) {
+  if (income < 100 || isNaN(income)) {
     showElementById("income-error");
+    return;
+  }
+
+  if (isNaN(software) || isNaN(courses) || isNaN(internet)) {
+    alert("Should be filled each input");
     return;
   }
 
@@ -42,49 +55,25 @@ document.getElementById("calculate").addEventListener("click", function () {
 
   showElementById("results");
 
+  count++;
+
   const historyContainer = document.getElementById("history-list");
   const historyItem = document.createElement("div");
   historyItem.className = "bg-white border-l-4 rounded-md p-4 border-blue-400";
   historyItem.innerHTML = `
-    <p class="text-xs text-gray-900">Serial: ${count}</p>
-    <p class="text-xs text-gray-900">${new Date().toLocaleDateString()}</p>
-    <p class="font-bold text-base text-gray-900">Income: ${totalExpenses.toFixed(
-      2
-    )}</p>
+  <p class="text-xs text-gray-900">Serial: ${count}</p>
+ <p class="text-xs text-gray-900">${new Date().toLocaleDateString()}</p>
+ <p class="font-bold text-base text-gray-900">Income: ${income.toFixed(2)}</p>
     <p class="text-xs text-gray-900">Expenses: ${totalExpenses.toFixed(2)}</p>
     <p class="text-xs text-gray-900">Balance: ${balance.toFixed(2)}</p>
-
-    `;
+  `;
   historyContainer.insertBefore(historyItem, historyContainer.firstChild);
-});
-
-document
-  .getElementById("calculate-savings")
-  .addEventListener("click", function () {
-    const savingPercentage = getInputValueById("savings");
-
-    const income = getInputValueById("income");
-    const software = getInputValueById("software");
-    const courses = getInputValueById("courses");
-    const internet = getInputValueById("internet");
-
-    const totalExpenses = software + courses + internet;
-    const balance = income - totalExpenses;
-
-    const savingAmount = (balance * savingPercentage) / 100;
-    const remainingBalance = balance - savingAmount;
-
-    const savingElement = document.getElementById("savings-amount");
-    savingElement.innerText = savingAmount.toFixed(2);
-
-    const remainingElement = document.getElementById("remaining-balance");
-    remainingElement.innerText = remainingBalance.toFixed(2);
-  });
+}
 
 const historyTab = document.getElementById("history-tab");
 const assistantTab = document.getElementById("assistant-tab");
 
-document.getElementById("history-tab").addEventListener("click", function () {
+historyTab.addEventListener("click", function () {
   historyTab.classList.add(
     "text-white",
     "font-semibold",
@@ -101,8 +90,8 @@ document.getElementById("history-tab").addEventListener("click", function () {
     "to-purple-600"
   );
 
-  hideElementById("expense-form");
   showElementById("history-section");
+  hideElementById("expense-form");
 });
 
 document.getElementById("assistant-tab").addEventListener("click", function () {
@@ -125,11 +114,29 @@ document.getElementById("assistant-tab").addEventListener("click", function () {
   hideElementById("history-section");
 });
 
-document.getElementById("income").addEventListener("input", function () {
-  const inputValue = getInputValueById("income");
-  if (inputValue <= 0) {
-    showElementById("income-error");
-  } else {
-    hideElementById("income-error");
-  }
-});
+document
+  .getElementById("calculate-savings")
+  .addEventListener("click", function () {
+    const savingPercentage = getInputValueById("savings");
+    const income = getInputValueById("income");
+    const software = getInputValueById("software");
+    const courses = getInputValueById("courses");
+    const internet = getInputValueById("internet");
+
+    const totalExpenses = software + courses + internet;
+    const balance = income - totalExpenses;
+
+    const savingAmount = (balance * savingPercentage) / 100;
+    const remainingBalance = balance - savingAmount;
+    const savingElement = document.getElementById("savings-amount");
+    savingElement.innerText = savingAmount.toFixed(2);
+
+    const remainingElement = document.getElementById("remaining-balance");
+    remainingElement.innerText = remainingBalance.toFixed(2);
+
+    document.getElementById("income").value = "";
+    document.getElementById("courses").value = "";
+    document.getElementById("software").value = "";
+    document.getElementById("internet").value = "";
+    document.getElementById("savings").value = "";
+  });
